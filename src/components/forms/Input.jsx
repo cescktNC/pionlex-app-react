a7e7b514636d4a35f0120274431db8938af157fc
Francesc Navarro <cesccat82@gmail.com>
2025-03-03 10:37:14 +0100

    feat(ui): added rounded corners style options to Input component (full, left, right, none)

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -3,17 +3,27 @@ import PropTypes from 'prop-types';
 export default function Input({ 
   type = 'text', 
   className = '', 
+  rounded = 'full',
   name, 
   placeholder = '', 
   dataRef
 }) {
+
+  const roundedStyles = {
+    full: 'rounded-lg',
+    left: 'rounded-l-lg',
+    right: 'rounded-r-lg',
+    none: 'rounded-none',
+  };
+
   return (
     <input 
       type={type} 
       className={`
         block w-full p-4
         text-2xl font-medium text-dark-gray-600 dark:text-light-gray-600
-        border-2 border-solid border-light-gray-600 rounded-lg 
+        border-2 border-solid border-light-gray-600
+        ${roundedStyles[rounded]}
         bg-white dark:bg-dark-gray-600
         transition-colors 
         placeholder:text-light-gray-600 dark:placeholder:text-light-gray-600 
@@ -31,6 +41,7 @@ export default function Input({
 Input.propTypes = {
   type: PropTypes.string,
   className: PropTypes.string,
+  rounded: PropTypes.oneOf(['full', 'left', 'right', 'none']),
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   dataRef: PropTypes.oneOfType([
