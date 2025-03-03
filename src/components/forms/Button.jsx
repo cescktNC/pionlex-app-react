efdea4a58641c8005c2b682eb61c42556730dc16
Francesc Navarro <cesccat82@gmail.com>
2025-03-03 10:32:05 +0100

    feat(ui): added rounded corners style options to Button component (full, left, right, none)

--- src/components/forms/Button.jsx
+++ src/components/forms/Button.jsx
@@ -7,15 +7,24 @@ export default function Button({
   type = 'button', 
   className = '', 
   variant = 'primary', 
+  rounded = 'full',
   disabled = false, 
   loading = false,
   onClick,
   to
 }) {
+  
   const variants = {
     primary: 'text-dark-gray-600 bg-pionlex-primary-400 hover:bg-pionlex-primary-600 disabled:hover:bg-pionlex-primary-400',
     secondary: 'text-dark-gray-600 bg-slate-400 hover:bg-slate-500 disabled:hover:bg-slate-400',
     warning: 'text-dark-gray-600 bg-yellow-400 hover:bg-yellow-600 disabled:hover:bg-yellow-400',
+  };
+
+  const roundedStyles = {
+    full: 'rounded-lg',
+    left: 'rounded-l-lg',
+    right: 'rounded-r-lg',
+    none: 'rounded-none',
   };
 
   const buttonContent = (
@@ -25,9 +34,10 @@ export default function Button({
       disabled={disabled || loading}
       className={`
         flex items-center justify-center
-        py-3 px-8 rounded-lg w-fit
+        py-3 px-8 w-fit
         font-semibold text-2xl  
         ${variants[variant]}
+        ${roundedStyles[rounded]}
         disabled:opacity-50 disabled:cursor-not-allowed
         ${className}
         cursor-pointer transition-colors duration-300 ease-in-out`}>
@@ -44,6 +54,7 @@ Button.propTypes = {
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
   className: PropTypes.string,
   variant: PropTypes.oneOf(['primary', 'secondary', 'warning']),
+  rounded: PropTypes.oneOf(['full', 'left', 'right', 'none']),
   disabled: PropTypes.bool,
   loading: PropTypes.bool,
   onClick: PropTypes.func,
